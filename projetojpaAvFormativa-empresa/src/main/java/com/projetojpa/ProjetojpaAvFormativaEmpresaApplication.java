8c5f65fdfa778b954468cf27a0ef139f25ca25bb
giovana1806 <132360035+giovana1806@users.noreply.github.com>
2023-11-16 11:33:45 -0300

    Add files via upload

---
+++ projetojpaAvFormativa-empresa/src/main/java/com/projetojpa/ProjetojpaAvFormativaEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaAvFormativaEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaAvFormativaEmpresaApplication.class, args);
+	}
+
+}
